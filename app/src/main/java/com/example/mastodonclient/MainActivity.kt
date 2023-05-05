eea01c02f847c6cc5dec1399b9ed6c748430ce7a
yudai-shimoma <cdcdasdll@icloud.com>
2023-05-05 22:06:28 +0900

    shimoma : 2023/05/5 	add : Step5 Fragmentを表示する 		・ライブラリ（Fragment-KTK）の追加 		・Fragmentで表示するレイアウトを作成 		・Activityの」レイアウトにFragmentを表示するViewを追加 		・Fragmentクラスを作成 		・Fragmentの表示

--- app/src/main/java/com/example/mastodonclient/MainActivity.kt
+++ app/src/main/java/com/example/mastodonclient/MainActivity.kt
@@ -1,40 +1,22 @@
 package com.example.mastodonclient
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.databinding.DataBindingUtil
-import com.example.mastodonclient.databinding.MainActivityBinding
-//import android.widget.TextView
-//import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.main_activity)
 
-//        val textView: TextView = findViewById(R.id.textview)
-//        textView.text = "Hello XML Layout!"
-        val binding: MainActivityBinding = DataBindingUtil.setContentView(
-            this,
-            R.layout.main_activity
-        )
-        binding.textview.text = "Hello DataBinding!"
+        if (savedInstanceState == null) {
+            val fragment = MainFragment()
+            supportFragmentManager.beginTransaction()
+                .add(
+                    R.id.fragment_container,
+                    fragment,
+                    MainFragment::class.java.simpleName
+                )
+                .commit()
+        }
     }
 }
-//
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    MastodonClientTheme {
-//        Greeting("Android")
-//    }
-//}
