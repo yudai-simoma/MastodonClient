bb16f101fe9527426a9ba39e36db3147defd068d
yudai-shimoma <cdcdasdll@icloud.com>
2023-05-06 19:44:10 +0900

    shimoma : 2023/05/6 	add : Step13 添付画像を表示する 		・ライブラリの追加（glide) 		・BindingAdapterを追加 		・レイアウトの変更

--- app/src/main/java/com/example/mastodonclient/DataBindingAdapter.kt
+++ app/src/main/java/com/example/mastodonclient/DataBindingAdapter.kt
@@ -3,6 +3,8 @@ package com.example.mastodonclient
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 //メソッドをDataBindingからspannedContent属性として利用する
 @BindingAdapter("spannedContent")
@@ -13,4 +15,19 @@ fun TextView.setSpannedString(content: String) {
         content,
         HtmlCompat.FROM_HTML_MODE_COMPACT
     )
+}
+
+//DateBindingからmedia属性として利用
+@BindingAdapter("media")
+//ImageViewクラスを拡張してsetMediaメソッドを追加
+fun ImageView.setMedia(media: Media?) {
+    //mediaがnullであれば（添付画像が存在しない）。ImageViewをクリアする
+    if (media == null) {
+        setImageDrawable(null)
+        return
+    }
+    //Glideによる画像のダウンロードとImageViewへの表示
+    Glide.with(this)
+        .load(media.url)
+        .into(this)
 }
