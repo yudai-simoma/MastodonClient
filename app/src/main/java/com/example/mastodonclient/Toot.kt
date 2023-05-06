d24dbfb9080e9d9c79e73ad0649fa0a69a5369ca
yudai-shimoma <cdcdasdll@icloud.com>
2023-05-06 19:28:40 +0900

    shimoma : 2023/05/6 	add : Step12 添付画像を表示するための準備 		・Mediaの取得

--- app/src/main/java/com/example/mastodonclient/Toot.kt
+++ app/src/main/java/com/example/mastodonclient/Toot.kt
@@ -8,7 +8,13 @@ data class Toot(
     @Json(name = "created_at") val createdAt: String,
     val sensitive: Boolean,
     val url: String,
+    //Mediaのリストを追加
+    @Json(name = "media_attachments") val mediaAttachments: List<Media>,
     val content: String,
     //accountキーの示す連想配列をAccountクラスのオブジェクトに変換する
     val account: Account
-)
+) {
+    //リストから最初のMediaを取得。存在しない場合はnullを返す
+    val topMedia: Media?
+        get() = mediaAttachments.firstOrNull()
+}
