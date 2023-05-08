97684ab85b0fa80d9970b3eedc1d539c2e73a81c
yudai-shimoma <cdcdasdll@icloud.com>
2023-05-08 20:50:07 +0900

    shimoma : 2023/05/8 	add : Step19 Repositoryパターンの導入

--- app/src/main/java/com/example/mastodonclient/TootListFragment.kt
+++ app/src/main/java/com/example/mastodonclient/TootListFragment.kt
@@ -10,21 +10,11 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.example.mastodonclient.databinding.FragmentTootListBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.atomic.AtomicBoolean
-//本にはないが、SSL認証が切れているため無効にする設定に使用
-import okhttp3.OkHttpClient
-import java.security.cert.X509Certificate
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.X509TrustManager
 
 class TootListFragment : Fragment(R.layout.fragment_toot_list) {
 
@@ -36,38 +26,8 @@ class TootListFragment : Fragment(R.layout.fragment_toot_list) {
 
     private var binding: FragmentTootListBinding? = null
 
-    private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
-
     // SSL 証明書を検証しない OkHttpClient の設定
-    private val retrofit: Retrofit
-    private val api: MastodonApi
-
-    init {
-        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-            override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
-            override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
-            override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-        })
-
-        val sslContext = SSLContext.getInstance("SSL")
-        sslContext.init(null, trustAllCerts, java.security.SecureRandom())
-
-        val okHttpClient = OkHttpClient.Builder()
-            .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
-            .hostnameVerifier { _, _ -> true }
-            .build()
-
-        //RetrofitでAPIにアクセスする準備。アクセス先のURLとAPIの定義を指定して初期化する
-        retrofit = Retrofit.Builder()
-            .baseUrl(API_BASE_URL)
-            .client(okHttpClient) // ここでSSL認証無効の設定を適用
-            //Moshiを使ってJSONをパースするようにRetrofitに登録（追加）
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-        api = retrofit.create(MastodonApi::class.java)
-    }
+    private val tootRepository = TootRepository(API_BASE_URL)
 
     private lateinit var adapter: TootListAdapter
     private lateinit var layoutManager: LinearLayoutManager
@@ -162,12 +122,10 @@ class TootListFragment : Fragment(R.layout.fragment_toot_list) {
             val tootListSnapshot = tootList.value ?: return@launch
 
             //ネットワーク接続処理はI/O用のスレッドを指定
-            val tootListResponse = withContext(Dispatchers.IO) {
-                api.fetchPublicTimeline(
-                    maxId = tootListSnapshot.lastOrNull()?.id,
-                    onlyMedia = true
-                )
-            }
+            val tootListResponse = tootRepository.fetchPublicTimeline(
+                maxId = tootListSnapshot.lastOrNull()?.id,
+                onlyMedia = true
+            )
             Log.d(TAG, "fetchPublicTimeline")
 
             tootListSnapshot.addAll(tootListResponse.filter { !it.sensitive })
---
+++ app/src/main/java/com/example/mastodonclient/TootRepository.kt
@@ -0,0 +1,60 @@
+package com.example.mastodonclient
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
+class TootRepository(
+    instanceUrl: String
+) {
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit: Retrofit
+    private val api: MastodonApi
+
+    init {
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+            override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+            override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+        })
+
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+
+        val okHttpClient = OkHttpClient.Builder()
+            .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }
+            .build()
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(instanceUrl)
+            .client(okHttpClient) // ここでSSL認証無効の設定を適用
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+        api = retrofit.create(MastodonApi::class.java)
+    }
+
+    suspend fun fetchPublicTimeline(
+        maxId: String?,
+        onlyMedia: Boolean
+    //実行スレッドをIOに指定。呼び出し側は実行スレッドを意識する必要がない
+    ) = withContext(Dispatchers.IO) {
+        api.fetchPublicTimeline(
+            maxId = maxId,
+            onlyMedia = onlyMedia
+        )
+    }
+
+}
