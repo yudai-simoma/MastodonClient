6499b8fefd842cf52bbed7a4b57be7fec519207e
yudai-shimoma <cdcdasdll@icloud.com>
2023-05-13 16:08:44 +0900

    shimoma : 2023/05/13 	add : Step23 開発者用アクセストークンを取得する 		・アプリの登録と開発者用のアクセストークン 		・アクセストークンの格納と読み込み

--- app/src/main/java/com/example/mastodonclient/TootListFragment.kt
+++ app/src/main/java/com/example/mastodonclient/TootListFragment.kt
@@ -16,8 +16,6 @@ class TootListFragment : Fragment(R.layout.fragment_toot_list) {
 
     companion object {
         val TAG = TootListFragment::class.java.simpleName
-
-        private const val API_BASE_URL = "https://androidbook2020.keiji.io"
     }
 
     private var binding: FragmentTootListBinding? = null
@@ -28,7 +26,8 @@ class TootListFragment : Fragment(R.layout.fragment_toot_list) {
     //viewModelを生成
     private val viewModel: TootListViewModel by viewModels {
         TootListViewModelFactory(
-            API_BASE_URL,
+            //APIURL
+            BuildConfig.INSTANCE_URL,
             lifecycleScope,
             requireContext()
         )
