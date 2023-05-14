c392de67c79a193077bf83e20a9ab5dcd982a615
yudai-shimoma <cdcdasdll@icloud.com>
2023-05-14 13:19:10 +0900

    shimoma : 2023/05/14 	add : Step30 下メニュー（BottomNavigation）を追加する 		・ライブラリの追加（Material Components） 		・スタイルの変更 		・レイアウトXMLの変更 		・メニューXMLの作成 		・Fragmentの切り替え 		・メニューアイコンを設定

--- app/src/main/java/com/example/mastodonclient/ui/MainActivity.kt
+++ app/src/main/java/com/example/mastodonclient/ui/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.example.mastodonclient.ui
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import com.example.mastodonclient.R
+import com.example.mastodonclient.databinding.MainActivityBinding
 import com.example.mastodonclient.ui.toot_list.TootListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        val binding: MainActivityBinding =
+            DataBindingUtil.setContentView(this, R.layout.main_activity)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            val fragment = when (it.itemId) {
+                R.id.menu_home -> TootListFragment()
+                R.id.menu_public -> TootListFragment()
+                else -> null
+            }
+            fragment ?: return@setOnNavigationItemSelectedListener false
+
+            supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.fragment_container,
+                    fragment,
+                    TootListFragment.TAG
+                )
+                .commit()
+
+            return@setOnNavigationItemSelectedListener true
+        }
 
         if (savedInstanceState == null) {
             val fragment = TootListFragment()
