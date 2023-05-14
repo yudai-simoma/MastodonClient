40634ea1192610a75fa99afdc7160a2e0a22a2a3
yudai-shimoma <cdcdasdll@icloud.com>
2023-05-14 11:39:45 +0900

    shimoma : 2023/05/14 	add : Step28 詳細画面に全ての画像を表示する 		・レイアウトXMLを作成 		・RecyclerViewに設定するAdapterを作成 		・レイアウトXMLを変更 		・FragmentでRacyclerViewを表示

--- app/src/main/java/com/example/mastodonclient/TootDetailFragment.kt
+++ app/src/main/java/com/example/mastodonclient/TootDetailFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mastodonclient.databinding.FragmentTootDetailBinding
 
 class TootDetailFragment : Fragment(R.layout.fragment_toot_detail) {
@@ -50,6 +51,8 @@ class TootDetailFragment : Fragment(R.layout.fragment_toot_detail) {
         }
     }
 
+    private lateinit var adapter : MediaListAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -61,8 +64,20 @@ class TootDetailFragment : Fragment(R.layout.fragment_toot_detail) {
             return
         }
 
+        bindingData.recyclerView.layoutManager = LinearLayoutManager(
+            requireContext(),
+            //要素を水平方向に並べる指定
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        bindingData.recyclerView.adapter = MediaListAdapter(layoutInflater).also {
+            adapter = it
+        }
+
         viewModel.toot.observe(viewLifecycleOwner, Observer {
             bindingData.toot = it
+            //メディア一覧を表示
+            adapter.mediaList = it.mediaAttachments
         })
     }
 
